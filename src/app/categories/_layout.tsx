04a07c9b05f297b6238f7d10d1de903e8192f066
martinromario55 <martinromario55@gmail.com>
2024-10-28 21:19:18 +0300

    Layouts

---
+++ src/app/categories/_layout.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const CategoriesLayout = () => {
+  return (
+    <View>
+      <Text>CategoriesLayout</Text>
+    </View>
+  )
+}
+
+export default CategoriesLayout
---
+++ src/app/product/_layout.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const ProductLayout = () => {
+  return (
+    <View>
+      <Text>ProductLayout</Text>
+    </View>
+  )
+}
+
+export default ProductLayout
