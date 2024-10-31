c880b6db00b72bc7b0e7c6a06b19a5981c18193f
martinromario55 <martinromario55@gmail.com>
2024-10-31 22:49:36 +0300

    more screens

---
+++ src/app/(shop)/tabs.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const tabs = () => {
+  return (
+    <View>
+      <Text>tabs</Text>
+    </View>
+  )
+}
+
+export default tabs
