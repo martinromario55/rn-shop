a5688f918ae144522f4ccbf6157664094113d78f
martinromario55 <martinromario55@gmail.com>
2024-11-04 21:11:15 +0300

    shop data

---
+++ src/app/(shop)/api.ts
@@ -0,0 +1,9 @@
+const fetchjsonShopData = async () => {
+  try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts')
+    const data = await response.json()
+    console.log(data)
+  } catch (error) {
+    console.error('Error:', error)
+  }
+}
